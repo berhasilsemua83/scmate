67e52f143833b27532464ce5b5e3b15cee031a7f
berhasilsemua83 <90000413+berhasilsemua83@users.noreply.github.com>
2026-08-05 09:28:40 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1377,7 +1377,7 @@ useEffect(() => {
             onChange={(e) => setApiKeyInput(e.target.value)}
             className="w-full bg-slate-800 border border-slate-700 rounded-md shadow-sm px-4 py-2 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 transition text-center mb-2"
           />
-          <p className="text-xs text-slate-500">
+          <p className="text-xs text-slate-500 text-center">
             Jika Kuota Server Habis masukan APIkey Anda sendiri secara manual di atas, API Key hanya tersimpan di browser Anda. Cara mendapatkan apikey sendiri silakan buka di 
             <a href="https://www.akariu.my.id/p/buat-apikey-google-gratis.html" target="_blank" rel="noopener noreferrer" className="text-indigo-400 hover:underline mx-1">
               sini
@@ -1388,6 +1388,7 @@ useEffect(() => {
             </a> 
             dari Google.
           </p>
+        </div>
       </header>
 
       {/* --- TOP LEVEL TABS --- */}
